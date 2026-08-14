8b3ba3a17b41829170a07d26b068a2d7990b92b2
1oganReed <reedchristopherlogan@gmail.com>
2026-08-14 07:44:12 -0700

    Save current changes

--- pages/checkout/checkout.js
+++ pages/checkout/checkout.js
@@ -2,4 +2,8 @@ function purchaseBtn() {
     document.getElementById("nextbtn")
 }
 
+function purchaseBtn() {
+    document.getElementById("nextbtn")
+}
+
 document.getElementById("menuitems").innerHTML = "";
