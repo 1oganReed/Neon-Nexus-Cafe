af4e4058c6d20233d18449d33d43d1397b8fdedf
1oganReed <reedchristopherlogan@gmail.com>
2026-08-26 08:29:24 -0700

    Animation on enter

--- homepage.js
+++ homepage.js
@@ -1,19 +1,4 @@
-// Following options represent the defaults
-const options = {
-  // Whether or not the true cursor should be hidden when the custom cursor is initialized
-  hideTrueCursor: false,
 
-  // Array of DOM elements/selector strings that add interactions on hover
-  focusElements: ["a", "button"],
-
-  // Class applied when the true cursor is hovering over a focusElement
-  focusClass: "cursor--focused",
-};
-
-// Can be selector string or DOM node
-const element = ".cursor";
-
-/* const customCursor = new CustomCursor(element, options); */
 
 
 // Script for card flips.
